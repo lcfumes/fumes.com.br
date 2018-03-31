559084177e0efbba8f13367bad933e0e6fabf888
Luiz Fumes <lcfumes@gmail.com>
2018-03-31 03:31:53 -0300

    First commit Added ssh key

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env sh
 set -x
 
-tar -czf package.tgz build && \
-	scp package.tgz deploy@$fumes.com.br:/srv/fumes.travis.com.br && \
-	ssh deploy@fumes.com.br 'bash -s' < ./scripts/untar.sh
+gulp build && \
+mkdir dist/scripts && \
+cp ./scripts/* dist/scripts/ && \
+cd dist && \
+tar -czf package.tgz * && \
+scp package.tgz deploy@fumes.com.br:/srv/fumes.com.br && \
+ssh deploy@fumes.com.br 'bash -s' < ./scripts/untar.sh && \
+cd .. && \
+gulp clean:dist
--- scripts/untar.sh
+++ scripts/untar.sh
@@ -4,7 +4,7 @@ set -x
 export NODE_ENV=production
 export NVM_BIN=$HOME/.nvm/versions/node/v6.9.0/bin
 
-cd /srv/fumes.travis.com.br && \
-	tar zxvf package.tgz -C . && \
-	mv build/package.json . && \
-	npm install
+cd /srv/fumes.com.br && \
+tar zxvf package.tgz -C . && \
+rm package.tgz && \
+rm -rf scripts
